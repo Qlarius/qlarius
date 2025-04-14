1835382dd9eccb40ac9d8e53d2969841c7f70e7d
George <george@arrowsmith.codes>
2025-04-14 21:30:37 +0100

    make faker available in prod

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Qlarius.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:faker, "~> 0.18.0", only: :dev},
+      {:faker, "~> 0.18.0"},
       {:nimble_csv, "~> 1.2"},
       {:cors_plug, "~> 3.0"}
     ]
